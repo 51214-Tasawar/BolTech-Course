f374ecb68059902a823504fda8bc0cba3c3a0b44
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-26 11:55:48 +0500

    Update Code

--- BootCamp Course Outline/JavaScript/1stprogramm.js
+++ BootCamp Course Outline/JavaScript/1stprogramm.js
@@ -1,6 +1,12 @@
 console.log("Test the Programm run at Console");    
-  
 a = 10 ;
 b = 20 ;
 c = a + b ;
 console.log(c);
+age = 20 ;
+if(age>=18){
+    console.log(`You are Eligible For this Opportunity ${age}`)
+}
+else {
+    console.log(`You are not Eligible For this Opportunity ${age}`)
+}
